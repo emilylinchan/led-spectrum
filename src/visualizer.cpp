2f32629cbbcdf85516f69014fdb30a701b945ba1
Joe Independent Developer <johnmilson125@gmail.com>
2026-05-07 11:06:46 +0100

    Update visualizer.cpp

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -17,6 +17,21 @@
 */
 
 #include "../inc/ui/visualizer.hpp"
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+#include <cmath>
+#include <conio.h>
+
+#define AsciiRgb(k, r, g, b) "\033[" #k ";2;" #r ";" #g ";" #b "m"
+
+template <typename _divide_number_t, typename _divide_number1_t>
+inline _divide_number_t __cdecl SafeDivide(_In_ _divide_number_t a, _In_ _divide_number1_t b) noexcept 
+{
+    if (b == 0) {return(1.00f);} return(_divide_number_t(_divide_number1_t(a)/b));
+}
 
 void RenderEqualizer::Display() {
     int level;
@@ -42,21 +57,39 @@ void RenderEqualizer::DisplayBuffer() {
     }
 }
 
-void RenderEqualizer::EnableVisualizer(std::vector<double>& freq, std::mutex& magMutex, int sampleRate) 
+void RenderEqualizer::EnableVisualizer(std::vector<double>& freq, std::mutex& magMutex, int sampleRate, const JsonFileReader& jsonFileReader) 
 {
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    termWidth  = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    termWidth  = csbi.srWindow.Right - csbi.srWindow.Left;
     termHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
     N_BARS = (termWidth - 1) / 2;
 
     unsigned int frameCount = 0;
     std::string frame;
+    std::string menu;
     frame.reserve(termWidth * termHeight);
     std::vector<float> barValues;
     barValues.resize(N_BARS);
 
-    SetConsoleOutputCP(CP_UTF8);
+    const struct ThemeModeManager themeModeManager[] = {
+        {"Default Mode", ThemeMode::Mode0},
+        {"Mode 1", ThemeMode::Mode1},
+        {"Mode 2", ThemeMode::Mode2},
+        {"Mode 3", ThemeMode::Mode3},
+        {"Mode 4", ThemeMode::Mode4}
+    };
+    size_t sizeOfTMM = sizeof(themeModeManager) / sizeof(const struct ThemeModeManager);
+    enum ThemeMode themeMode = ThemeMode::Mode0;
+
+    size_t tmIndex = 0;
+    while (tmIndex < sizeOfTMM) {
+        if (strcmp(themeModeManager[tmIndex].themeModeName, jsonFileReader.currentTheme.themeMode) == 0) {
+            themeMode = themeModeManager[tmIndex].themeMode;
+            break;
+        }
+        tmIndex++;
+    }
     
     // switch to alternate screen buffer and hide cursor
     // \033[?1049h: switch to alternate buffer
@@ -66,24 +99,25 @@ void RenderEqualizer::EnableVisualizer(std::vector<double>& freq, std::mutex& ma
     std::cout << "\033[?1049h\033[H\033[2J\033[?25l" << std::flush;
 
     while (AudioEngine::Get().IsRunning()) {
-
-        if (frameCount % 30 == 0) {
-            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-            int newWidth = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-            int newHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        termWidth  = csbi.srWindow.Right - csbi.srWindow.Left;
+        termHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        if (termHeight != lastHeight || termWidth != lastWidth) {
+            lastHeight = termHeight;
+            lastWidth = termWidth;
             
-            if (newWidth != termWidth || newHeight != termHeight) {
-                termWidth = newWidth;
-                termHeight = newHeight;
-                N_BARS = (termWidth - 1) / 2;
-                if (N_BARS < 1) N_BARS = 1;
-                barValues.resize(N_BARS);
-                frame.reserve(termWidth * termHeight);
-                // clear alternate screen on resize
-                std::cout << "\033[2J" << std::flush;
-            }
+            N_BARS = (termWidth - 1) / 2;
+            if (N_BARS < 1) N_BARS = 1;
+            barValues.resize(N_BARS);
+            frame.reserve(termWidth * termHeight);
+            // clear alternate screen on resize
+            std::cout << "\033[2J" << std::flush;
         }
-        frameCount++;
+        if (frameCount < UINT_MAX) {
+            frameCount++;
+        } else {
+            frameCount = 0;
+        }
 
         float masterVol = AudioEngine::Get().GenVolLevel();
 
@@ -101,8 +135,8 @@ void RenderEqualizer::EnableVisualizer(std::vector<double>& freq, std::mutex& ma
                     int binLow  = (int)(fLow * 1201 / (sampleRate / 2.f));
                     int binHigh = (int)(fHigh * 1201 / (sampleRate / 2.f));
                     
-                    binLow  = std::max(0, std::min(binLow, 1200));
-                    binHigh = std::max(0, std::min(binHigh, 1200));
+                    binLow  = max(0, min(binLow, 1200));
+                    binHigh = max(0, min(binHigh, 1200));
                     if (binLow >= binHigh) binHigh = binLow + 1;
                     if (binHigh > (int)freq.size()) binHigh = (int)freq.size();
                     
@@ -132,34 +166,137 @@ void RenderEqualizer::EnableVisualizer(std::vector<double>& freq, std::mutex& ma
         }
 
         frame.clear();
+        menu.clear();
+
+        if (jsonFileReader.showMenu) {
+            const char* volumeLevels[] = {
+                "[]--------------------",
+                "=[]-------------------",
+                "==[]------------------",
+                "===[]-----------------",
+                "====[]----------------",
+                "=====[]---------------",
+                "======[]--------------",
+                "=======[]-------------",
+                "========[]------------",
+                "=========[]-----------",
+                "==========[]---------",
+                "===========[]--------",
+                "============[]-------",
+                "=============[]------",
+                "==============[]-----",
+                "===============[]----",
+                "================[]---",
+                "=================[]--",
+                "==================[]-",
+                "===================[]",
+            };
+
+            menu += AsciiRgb(48, 0, 25, 23) AsciiRgb(38, 0, 255, 236) " * ╭─╮╭─╮╭─╴╭─╴╶┬╴╭─╮╷ ╷╭┬╮ * Master Volume: ";
+
+            int levelNumber = int((masterVol > 1) ? 1.0f : masterVol * int((sizeof(volumeLevels)  - sizeof(volumeLevels[0])) / sizeof(volumeLevels[0])));
+            menu += volumeLevels[levelNumber];
+
+            menu += "(";
+            menu += std::to_string(int(masterVol * 100));
+            menu += ") |";
+
+            menu += "\033[0m";
+            menu += "\n";
+
+            menu += AsciiRgb(48, 0, 18, 25) AsciiRgb(38, 0, 180, 255)  " = ╰─╮├─╯├╴ │   │ ├┬╯│ ││││ = \n";
+            menu += AsciiRgb(48, 0, 7, 25) AsciiRgb(38, 0, 73, 255) " * ╰─╯╵  ╰─╴╰─╴ ╵ ╵╰╴╰─╯╵ ╵ * \n";
+        }
         // use termHeight - 1 to allow bars to touch the top row
-        int renderHeight = termHeight - 1;
+        int added = (jsonFileReader.showMenu) ? 3 : 1;
+        int renderHeight = termHeight - added;
         if (renderHeight < 1) renderHeight = 1;
+
+        const char* UTF8Codes[] = {
+            "─", "│", "─", "│", "┌", "┐", "└", "┘", "├", "┤", "┬", "┴", "┼",
+            "♪", "♫", "♬", "♩", "⣿", "⣶", "⣤", "⣀", "⡇", "#", "a", "b", "c", "e",
+            "f", "g", "h"
+        };
+        size_t sizeofCodes = sizeof(UTF8Codes) / sizeof(UTF8Codes[0]);
 
         float rowA = 0;
         float rowB = 0;
         float rowC = 0;
+        float effect = 1.0f;
         for (int row = renderHeight; row > 0; row--) {
             for (int i = 0; i < N_BARS; i++) {
                 int barHeight = (int)(barValues[i] * renderHeight);
 
-                if (row <= barHeight) {
-                    char tempArray[256];
-                    unsigned char color8Bit[3] = {(unsigned char)(255 * rowA), (unsigned char)(255 * rowB), (unsigned char)(255 * rowC)};
-                    sprintf_s(tempArray, sizeof(tempArray), "\033[38;2;%d;%d;%dm:=\033[0m", color8Bit[0], color8Bit[1], color8Bit[2]);
+                if (themeMode == ThemeMode::Mode4) {
+                    effect = barValues[i] * 2.5f;
+                }
+
+                char tempArray[64];
+                char tempArray1[64];
+                int color8BitRaw[3] = {
+                    (int)((255 * rowA * effect) * (float)jsonFileReader.currentTheme.colorRed), 
+                    (int)((255 * rowB * effect) * (float)jsonFileReader.currentTheme.colorGreen), 
+                    (int)((255 * rowC * effect) * (float)jsonFileReader.currentTheme.colorBlue)};
+
+                unsigned char color8Bit[3] = {
+                    (color8BitRaw[0] > 255) ? (unsigned char)255 : (unsigned char)color8BitRaw[0],
+                    (color8BitRaw[1] > 255) ? (unsigned char)255 : (unsigned char)color8BitRaw[1],
+                    (color8BitRaw[2] > 255) ? (unsigned char)255 : (unsigned char)color8BitRaw[2]
+                };
+                rowA = float(row) / float(renderHeight);
+                rowB = -rowA + 1.0f;
+                rowC = -fabs(rowA - 0.5f) + 1.0f;
+
+                int barTop, barBottom;
+                if (themeMode == ThemeMode::Mode1) {
+                    barTop = (int)((renderHeight - barHeight) / 2);
+                    barBottom = (int)(renderHeight - ((renderHeight - barHeight) / 2));
+                }
+
+                if (themeMode == ThemeMode::Mode2) {
+                    barTop = barHeight;
+                    barBottom = barHeight;
+                }
+
+                if (themeMode == ThemeMode::Mode3) {
+                    int barHeight1 = renderHeight - barHeight;
+                    barTop = (int)((renderHeight - barHeight1) / 2);
+                    barBottom = (int)(renderHeight - ((renderHeight - barHeight1) / 2));
+                }
+
+                char character[2] = {jsonFileReader.currentTheme.customCharacter, '\0'};
+                
+                sprintf_s(tempArray, sizeof(tempArray), "\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm:%s\033[0m", color8Bit[0], color8Bit[1], color8Bit[2], 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[0], ((16 * rowA) + 8)), 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[1], ((16 * rowA) + 8)), 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[2], ((16 * rowA) + 8)), (jsonFileReader.currentTheme.useRandomCharacters) ? UTF8Codes[rand() % sizeofCodes] : character);
+                
+                sprintf_s(tempArray1, sizeof(tempArray1), "\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm  \033[0m", color8Bit[0], color8Bit[1], color8Bit[2], 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[0], ((16 * rowA) + 8)), 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[1], ((16 * rowA) + 8)), 
+                    SafeDivide<unsigned char, unsigned char>(color8Bit[2], ((16 * rowA) + 8)));
+
+                if (themeMode == ThemeMode::Mode0) { 
+                    if (row <= barHeight) {
+                        frame += tempArray;
+                    } else {
+                        frame += (!jsonFileReader.noBgColor) ? tempArray1 : "\033[0m  ";
+                    }
+                } else if (themeMode == ThemeMode::Mode1 || themeMode == ThemeMode::Mode2 || themeMode == ThemeMode::Mode3) {
+                    if (row <= barBottom && row >= barTop) {
+                        frame += (themeMode == ThemeMode::Mode1 || themeMode == ThemeMode::Mode2) ? tempArray : tempArray1;
+                    } else {
+                        frame += (!jsonFileReader.noBgColor) ? ((themeMode == ThemeMode::Mode1 || themeMode == ThemeMode::Mode2) ? tempArray1 : tempArray) : "\033[0m  ";
+                    }
+                } else if (themeMode == ThemeMode::Mode4) {
                     frame += tempArray;
-                } else {
-                    frame += "\033[38;2;37;37;37m:=\033[0m";
-                    rowA = float(row) / float(renderHeight);
-                    rowB = -rowA + 1.0f;
-                    rowC = -fabs(rowA - 0.5f);
                 }
             }
             if (row > 1) frame += '\n'; 
         }
 
         // Use \033[H to jump to top-left, \033[J to clear what's below the new frame
-        std::cout << "\033[H" << frame << "\033[J" << std::flush;        
+        std::cout << "\033[H" << menu << frame << "\033[J" << std::flush;        
         std::this_thread::sleep_for(std::chrono::milliseconds(16)); 
     }
     
